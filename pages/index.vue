df441313a00300718d253346099addc18f6c5aa4
Yee Thong <yeethong1995@gmail.com>
2022-08-04 20:03:11 +0800

    First change to test on git

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Finally problem solve to lunch the web page</h1>
+      <p class="mt-5">A good start on lunching the web is time to think on the template</p>
     </div>
   </div>
 </template>
