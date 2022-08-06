14d3414bf680126fd47013b9482f538f5d935d34
Yee Thong <yeethong1995@gmail.com>
2022-08-06 17:52:17 +0800

    Creating header menu and adding personal description

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Finally problem solve to lunch the web page</h1>
-      <p class="mt-5">A good start on lunching the web is time to think on the template</p>
+  <div id = "Profile section" class ="bg-[url('/profile-bg.jpg')] px-20" >
+    <div id = "Menu" class = "grid grid-cols-2 py-5">
+      <div id = "Icon">
+        <img src = "YT Icon.png" alt = "" width ="70" height="70">
+      </div>
+
+      <div id = "Header-Menu">
+        <ul class = "grid grid-cols-3 uppercase text-2xl text-white py-5">
+          <li>About me</li>
+          <li>My Project</li>
+          <li>Learn with me</li>
+        </ul>
+      </div>
+    </div>
+    <div id = "Profile-Section-Col-Style" class = "grid grid-cols-2 py-10">
+      <div id = "Profile-Description" class = "text-white text-center mt-10">
+        <h1 class = "text-5xl uppercase">Junior Data Analyst</h1>
+        <h2 class = "text-3xl uppercase my-5"> Yee Thong</h2>
+        <p>Hi I am Yee Thong you can call me YT in short. I am interested in solving data problem and visulize something out from the data.
+          This will be first webpage design for my own data science application. In my webpage I will also teach you some Python pandas trick on solving some data problem.
+        </p>
+      </div>
+      <div id ="Profile picture" class = "ml-20">
+        <img src = "Temporary Photo.jpg" alt = " " width = "300" height = "600">
+      </div>
     </div>
   </div>
 </template>
